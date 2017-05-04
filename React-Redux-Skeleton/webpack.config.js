5eb0d3d672dd2cfa96625cf58af4fd6901cad71d
ADS\mku <martin.kutz@zuehlke.com>
2017-05-04 14:19:56 +0200

    added experimental wepack vendor config (not working at the moment)

--- React-Redux-Skeleton/webpack.config.js
+++ React-Redux-Skeleton/webpack.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: {
+        bundle: [
+            "./src/index.tsx",
+        ]
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].js",
         path: __dirname + "/dist"
     },
-
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
@@ -30,7 +33,7 @@ module.exports = {
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: 'pre', test: /\.js$/, loader: "source-map-loader" }
+            { enforce: 'pre', test: /\.js$/, loader: "source-map-loader" },
         ]
     },
 
