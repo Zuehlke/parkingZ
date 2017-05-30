367c4193f66120ea888118f3ec74918072b3a6da
Manuel Pras <pram@zuehlke.com>
2017-05-30 15:45:46 +0200

    add level entry component and use fake data

--- React-Redux-Skeleton/src/components/App.tsx
+++ React-Redux-Skeleton/src/components/App.tsx
@@ -9,9 +9,63 @@ export class App extends React.Component<undefined, void> {
 
         const pageTitle = 'Welcome to the Parking Lot Management Tool';
 
+        const levels: Level[] = [
+            {
+                _id: "1",
+                level: 1,
+                name: "Ebene 1",
+                parkingLots: [
+                    {
+                        _id: "1",
+                        number: 1,
+                        status: 2,
+                        type: 0
+                    },
+                    {
+                        _id: "2",
+                        number: 2,
+                        status: 1,
+                        type: 0
+                    },
+                    {
+                        _id: "3",
+                        number: 2,
+                        status: 2,
+                        type: 0
+                    }
+                ]
+            },
+            {
+
+                _id: "2",
+                level: 2,
+                name: "Ebene 2",
+                parkingLots: [
+                    {
+                        _id: "1",
+                        number: 1,
+                        status: 0,
+                        type: 0
+                    },
+                    {
+                        _id: "2",
+                        number: 2,
+                        status: 1,
+                        type: 0
+                    },
+                    {
+                        _id: "3",
+                        number: 2,
+                        status: 1,
+                        type: 0
+                    }
+                ]
+            }
+        ]
+
         return (
             <div>
-                <ParkingLotCard></ParkingLotCard>
+                <ParkingLotCard title="Mein Titel" levels={levels}></ParkingLotCard>
             </div>
         );
     }
